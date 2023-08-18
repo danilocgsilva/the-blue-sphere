b05ef77728cef2c23a0f017b7a678773be3854ba
Danilo Silva <danilocgsilva@gmail.com>
2023-08-18 08:18:03 -0300

    Added sphere mesh

--- src/script.js
+++ src/script.js
@@ -2,10 +2,12 @@ import * as THREE from 'three'
 
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
-const geometry = new THREE.SphereGeometry(1, 32, 32);
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 
 const material = new THREE.MeshBasicMaterial()
 material.color = new THREE.Color(0xff0000)
 
+const sphereMesh = THREE.Mesh(sphereGeometry, material)
+scene.add(sphereMesh)
 
 alert('Still alive!')
